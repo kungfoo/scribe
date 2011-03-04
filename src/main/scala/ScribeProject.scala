2c150b934b5713b5a6d2326f19312639628873ae
Silvio Heuberger <silvio.heuberger@hsr.ch>
2011-03-04 20:12:19 +0100

    Added withProject() to replace the repetitive code. Added fake chunks() method to ScribeProject.

--- src/main/scala/ScribeProject.scala
+++ src/main/scala/ScribeProject.scala
@@ -1,11 +1,25 @@
 package ch.mollusca.scribe.project
 
 import java.io.File
+import java.io.FileInputStream
+
+import io.Source
+import io.BufferedSource
+
+import tfd.scala.yaml._
 
 class	ScribeProject(val folder: File) {
 	
 	lazy val name = folder.getName
+	lazy val projectFile = loadProjectFile
 	
+	def loadProjectFile : Source = {
+	  new BufferedSource(new FileInputStream(new File(folder, "project.yaml")))
+	}
+	
+	def chunks = List("small_chunk")
+	
+	def close = projectFile.close
 }
 
 object ScribeProject {
--- src/test/scala/ScribeProjectSpec.scala
+++ src/test/scala/ScribeProjectSpec.scala
@@ -6,12 +6,34 @@ import org.scalatest.matchers._
 import ch.mollusca.scribe.test._
 
 class ScribeProjectSpec extends FlatSpec with ShouldMatchers with LoadsTestResources {
-	"A ScribeProject" should "be able to find its project file" in {
-		val folder = createFolder("simplest_project")
-		val project = ScribeProject(folder)
-		project.name should equal("simplest_project")
+  def withProject(name: String)(function: ScribeProject => Unit) = {
+    val project = ScribeProject(createFolder(name))
+    try{
+      function(project)
+    } finally {
+      project.close
+    }
+  }
+
+  "A ScribeProject" should "be able to find its project file" in {
+    withProject("simplest_project") { project => 
+      val projectFile = project.projectFile
+      projectFile.length should be > (0)
+    }
 	}
 	
+	it should "be able to tell its name" in {
+	  withProject("simplest_project") { project =>
+	    project.name should equal ("simplest_project")
+	  }
+  }
+  
+  it should "be able to tell the paths of its chunks" in {
+    withProject("simplest_project") { project =>
+      val chunks = project.chunks
+      chunks should be (List("small_chunk"))
+    }
+  }
 }
 
 
